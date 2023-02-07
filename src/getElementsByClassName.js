efba50b1508358a4109f8fc56d4ea01f1dedbf16
Johnathan Simeroth <john.simeroth@gmail.com>
2023-02-06 20:30:44 -0800

    completed getElementsbyClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,15 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+// Declare result
+// Get class list of node
+// if class list includes "classname"
+//    concat body to result
+// Get all sub-elements in body
+// if there are no sub-elements (BASE CASE)
+//    return result
+// (if there are) Iterate through each sub-element (RECURSIVE CASE)
+//    return getElementsByClassName(className,current element)
 var getElementsByClassName = function(className, node) {
-  // Declare result
-  // Get node body
-  // Get class list of body
-  // if class list includes "classname"
-    // concat body to result
-  // Get all sub-elements in body
-  // if there are no sub-elements (BASE CASE)
-    // return result
-  // (if there are) Iterate through each sub-element (RECURSIVE CASE)
-    // return getElementsByClassName(className,current element)
+  var result = [];
+  if (!node) {
+    node = document.body;
+  }
+  if (node.classList.contains(className)) {
+    result = result.concat(node);
+  }
+  var children = node.children;
+  if (children.length === 0) {
+    return result;
+  }
+  children.forEach(function(child) {
+    result = result.concat(getElementsByClassName(className, child));
+  });
+  return result;
 };
