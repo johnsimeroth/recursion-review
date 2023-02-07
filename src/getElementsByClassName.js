544c9c5a0899c8258a47cfcf2c4f41f6b1cf4585
Noah Clouthier <noahclouthier@Noahs-MacBook-Air.local>
2023-02-06 19:47:30 -0800

    2/06/23 Pair Work 2/06/23 Pair Work

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  // Declare result
+  // Get node body
+  // Get class list of body
+  // if class list includes "classname"
+    // concat body to result
+  // Get all sub-elements in body
+  // if there are no sub-elements (BASE CASE)
+    // return result
+  // (if there are) Iterate through each sub-element (RECURSIVE CASE)
+    // return getElementsByClassName(className,current element)
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Create result string
+  var result = '';
+  // Framework defining target of both base case and recursive case
+
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    return undefined;
+  }
+  if (typeof obj !== 'object' || obj === null) {
+    if (typeof obj === 'string') {
+      result += '"' + obj + '"';
+      return result;
+    }
+    result += obj;
+    return result;
+  }
+
+  // is this an array?
+  if (Array.isArray(obj)) {
+    // is it empty?
+    if (obj.length === 0) {
+      // return '[]';
+      return '[]';
+    }
+    result += '[';
+    // do other array stuff (for loop here)
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    return result.slice(0, -1) + ']';
+  }
+
+  // must be object
+  // empty object?
+  if (Object.keys(obj).length === 0) {
+    return '{}';
+  }
+  // do other object stuff
+  result += '{';
+  for (var key in obj) {
+    if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+      result += '"' + key + '"' + ':';
+      result += stringifyJSON(obj[key]) + ',';
+    }
+  }
+  if (result === '{') {
+    return '{}';
+  }
+  return result.slice(0, -1) + '}';
 };
